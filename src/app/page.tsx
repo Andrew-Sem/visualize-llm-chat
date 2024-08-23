3d1c200ba90f399bd643d0971e01c82fe36ee1d2
Andrey Semenov <101043571+Andrew-Sem@users.noreply.github.com>
2024-08-23 21:16:12 +0300

    chore: shadcn/ui init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
-
 import { HydrateClient } from "@/trpc/server";
+import { Button } from "../components/ui/button";
 
 export default async function Home() {
   return (
     <HydrateClient>
       <main className="">
         <h1 className="text-5xl">Visualize llm chat</h1>
+        <Button>Hello world</Button>
       </main>
     </HydrateClient>
   );
