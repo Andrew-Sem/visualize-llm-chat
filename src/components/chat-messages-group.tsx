90e5a994522d0f5cc6a18f2e5ca6887d73ff28c4
Andrey Semenov <101043571+Andrew-Sem@users.noreply.github.com>
2024-08-24 12:29:15 +0300

    fix: selected messages ui

--- src/components/chat-messages-group.tsx
+++ src/components/chat-messages-group.tsx
@@ -10,35 +10,43 @@ export const ChatMessageGroup = ({
   messages: Message[];
   isUser: boolean;
 }) => (
-  <div className="w-full">
-    {messages.map(
-      (message, index) =>
-        message.content && (
-          <div
-            key={index}
-            className={cn(
-              "flex w-full items-end px-2 py-1",
-              message.selected ? "bg-muted" : "",
-            )}
-          >
-            {!isUser && (
-              <div className="mr-3 flex-shrink-0 self-end">
-                <SparklesIcon className="h-5 w-5 text-muted-foreground" />
+  <div className="relative flex w-full">
+    <div className="w-full">
+      {messages.map(
+        (message, index) =>
+          message.content && (
+            <div
+              key={index}
+              className={cn(
+                "flex w-full items-end",
+                message.selected ? "bg-muted" : "",
+              )}
+            >
+              {!isUser && (
+                <div className="sticky bottom-0 ml-3 flex-shrink-0 self-end p-2">
+                  {index === 0 && (
+                    <SparklesIcon className="h-5 w-5 text-muted-foreground" />
+                  )}
+                </div>
+              )}
+              <div className="flex-grow px-2 py-1">
+                <ChatMessageContent
+                  content={message.content}
+                  isUser={isUser}
+                  lastInGroup={index === messages.length - 1}
+                  isSelected={message.selected === true}
+                />
               </div>
-            )}
-            <ChatMessageContent
-              content={message.content}
-              isUser={isUser}
-              lastInGroup={index === messages.length - 1}
-              isSelected={message.selected === true}
-            />
-            {isUser && (
-              <div className="ml-3 flex-shrink-0 self-end rounded-full bg-muted p-2">
-                <UserRoundIcon className="h-5 w-5 text-muted-foreground" />
-              </div>
-            )}
-          </div>
-        ),
-    )}
+              {isUser && (
+                <div className="sticky bottom-0 mr-3 flex-shrink-0 self-end rounded-full bg-muted p-2">
+                  {index === 0 && (
+                    <UserRoundIcon className="h-5 w-5 text-muted-foreground" />
+                  )}
+                </div>
+              )}
+            </div>
+          ),
+      )}
+    </div>
   </div>
 );
--- src/components/chat-messsage-content.tsx
+++ src/components/chat-messsage-content.tsx
@@ -11,29 +11,22 @@ export const ChatMessageContent = ({
   lastInGroup: boolean;
   isSelected: boolean;
 }) => (
-  <div
-    className={cn(
-      "flex-grow",
-      isUser ? "flex justify-end" : "flex justify-start",
-    )}
-  >
-    <div className="relative">
-      <div
-        className={cn(
-          "break-words px-4 py-2 text-sm",
-          isUser
-            ? lastInGroup
-              ? "rounded-t-lg rounded-bl-lg bg-primary text-primary-foreground"
-              : "rounded-lg bg-primary text-primary-foreground"
-            : lastInGroup
-              ? "rounded-t-lg rounded-br-lg bg-secondary text-secondary-foreground"
-              : "rounded-lg bg-muted text-secondary-foreground",
-          isUser ? "ml-10" : "mr-10",
-          "max-w-xs lg:max-w-md",
-        )}
-      >
-        {content ?? "Unable to get content"}
-      </div>
+  <div className={cn("flex w-full", isUser ? "justify-end" : "justify-start")}>
+    <div
+      className={cn(
+        "relative break-words px-4 py-2 text-sm",
+        isUser
+          ? lastInGroup
+            ? "rounded-t-lg rounded-bl-lg bg-primary text-primary-foreground"
+            : "rounded-lg bg-primary text-primary-foreground"
+          : lastInGroup
+            ? "rounded-t-lg rounded-br-lg bg-secondary text-secondary-foreground"
+            : "rounded-lg bg-muted text-secondary-foreground",
+        isUser ? "ml-10" : "mr-10",
+        "max-w-xs lg:max-w-md",
+      )}
+    >
+      {content ?? "Unable to get content"}
       {lastInGroup ? (
         isUser ? (
           <>
@@ -50,7 +43,7 @@ export const ChatMessageContent = ({
             <div className="absolute -left-1 bottom-0 h-1.5 w-3 rounded-lg bg-secondary" />
             <div
               className={cn(
-                "absolute -left-3 bottom-0 h-3 w-3 rounded-full bg-background",
+                "absolute -left-3 bottom-0 h-3 w-3 rounded-full",
                 isSelected ? "bg-muted" : "bg-background",
               )}
             />
