46e8e0b642fb5955b446d8304d46352a6a6c95ee
Andrey Semenov <101043571+Andrew-Sem@users.noreply.github.com>
2024-08-24 17:26:33 +0300

    feat: base homepage

---
+++ src/app/mock-page/page.tsx
@@ -0,0 +1,69 @@
+import { ChatMessagesList } from "@/components/chat-messages-list";
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { mockMessages } from "@/mock/messages";
+import { HydrateClient } from "@/trpc/server";
+
+export default async function Home() {
+  return (
+    <HydrateClient>
+      <main className="md:container">
+        <Tabs defaultValue="chat" className="w-full md:hidden">
+          <TabsList className="w-full">
+            <TabsTrigger className="w-full" value="chat">
+              Chat
+            </TabsTrigger>
+            <TabsTrigger className="w-full" value="json">
+              Json
+            </TabsTrigger>
+          </TabsList>
+          <TabsContent value="chat">
+            <ScrollArea className="h-[85vh] py-0.5">
+              <ChatMessagesList messages={mockMessages} />
+            </ScrollArea>
+          </TabsContent>
+          <TabsContent value="json">
+            <ScrollArea className="h-[85vh] py-0.5">
+              <pre className="text-wrap text-sm">
+                {mockMessages
+                  .map((msg) => JSON.stringify(msg, null, 2))
+                  .join(",\n")}
+              </pre>
+            </ScrollArea>
+          </TabsContent>
+        </Tabs>
+        <div className="hidden md:block">
+          <ResizablePanelGroup
+            direction="horizontal"
+            className="rounded-lg border"
+          >
+            <ResizablePanel defaultSize={40} minSize={35}>
+              <ScrollArea className="h-[90vh] py-4">
+                <ChatMessagesList messages={mockMessages} />
+              </ScrollArea>
+            </ResizablePanel>
+            <ResizableHandle withHandle />
+            <ResizablePanel
+              defaultSize={50}
+              minSize={30}
+              className="hidden md:block"
+            >
+              <ScrollArea className="h-[90vh] p-4">
+                <pre className="text-wrap text-sm">
+                  {mockMessages
+                    .map((msg) => JSON.stringify(msg, null, 2))
+                    .join(",\n")}
+                </pre>
+              </ScrollArea>
+            </ResizablePanel>
+          </ResizablePanelGroup>
+        </div>
+      </main>
+    </HydrateClient>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,68 +1,21 @@
-import {
-  ResizableHandle,
-  ResizablePanel,
-  ResizablePanelGroup,
-} from "@/components/ui/resizable";
-import { ScrollArea } from "@/components/ui/scroll-area";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Badge } from "@/components/ui/badge";
 import { HydrateClient } from "@/trpc/server";
-import { ChatMessagesList } from "../components/chat-messages-list";
-import { mockMessages } from "../mock/messages";
+import { Button } from "../components/ui/button";
 
 export default async function Home() {
   return (
     <HydrateClient>
-      <main className="md:container">
-        <Tabs defaultValue="chat" className="w-full md:hidden">
-          <TabsList className="w-full">
-            <TabsTrigger className="w-full" value="chat">
-              Chat
-            </TabsTrigger>
-            <TabsTrigger className="w-full" value="json">
-              Json
-            </TabsTrigger>
-          </TabsList>
-          <TabsContent value="chat">
-            <ScrollArea className="h-[85vh] py-0.5">
-              <ChatMessagesList messages={mockMessages} />
-            </ScrollArea>
-          </TabsContent>
-          <TabsContent value="json">
-            <ScrollArea className="h-[85vh] py-0.5">
-              <pre className="text-wrap text-sm">
-                {mockMessages
-                  .map((msg) => JSON.stringify(msg, null, 2))
-                  .join(",\n")}
-              </pre>
-            </ScrollArea>
-          </TabsContent>
-        </Tabs>
-        <div className="hidden md:block">
-          <ResizablePanelGroup
-            direction="horizontal"
-            className="rounded-lg border"
-          >
-            <ResizablePanel defaultSize={40} minSize={35}>
-              <ScrollArea className="h-[90vh] py-4">
-                <ChatMessagesList messages={mockMessages} />
-              </ScrollArea>
-            </ResizablePanel>
-            <ResizableHandle withHandle />
-            <ResizablePanel
-              defaultSize={50}
-              minSize={30}
-              className="hidden md:block"
-            >
-              <ScrollArea className="h-[90vh] p-4">
-                <pre className="text-wrap text-sm">
-                  {mockMessages
-                    .map((msg) => JSON.stringify(msg, null, 2))
-                    .join(",\n")}
-                </pre>
-              </ScrollArea>
-            </ResizablePanel>
-          </ResizablePanelGroup>
-        </div>
+      <main className="container">
+        <section className="mt-20 text-center">
+          <h1 className="text-5xl lg:text-6xl">Visualize LLM chat</h1>
+          <div className="mt-2">
+            <Badge variant="outline">Why? Cause I can</Badge>
+          </div>
+          <div className="mt-10 space-x-4">
+            <Button>Create new</Button>
+            <Button variant={"outline"}>My vizualizations</Button>
+          </div>
+        </section>
       </main>
     </HydrateClient>
   );
